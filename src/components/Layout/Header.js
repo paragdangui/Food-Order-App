3dfd572228e1e4cb275cc3779ffb26cd2967ca72
paragdangui <paragdangui@gmail.com>
2021-07-26 12:22:40 +0530

    header cart icon added and styled

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 // file imports 
 import classes from './Header.module.css'
 import mealsImage from '../../assets/meals.jpg'
-
+import HeaderCartButton from './HeaderCartButton';
 const Header = () => {
 	return (
 		<React.Fragment>
@@ -11,7 +11,7 @@ const Header = () => {
 				<h1>
 					ReactMeals
 				</h1>
-				<button>Cart</button>
+				<HeaderCartButton />
 			</header>
 			<div className={classes['main-image']}>
 				<img src={mealsImage} alt="table full of food" />
