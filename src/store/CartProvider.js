be6aeff5d4fe4ca63a3f43e606257a934c8271e0
paragdangui <paragdangui@gmail.com>
2021-07-30 16:42:06 +0530

    remove functionality added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -31,6 +31,28 @@ const cartReducer = (state, action) => {
 		}
 
 
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount
+		}
+	}
+	if (action.type === 'REMOVE') {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+		const existingitem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingitem.price
+		let updatedItems;
+
+		if (existingitem.amount === 1) {
+			updatedItems = state.items.filter(item => item.id !== action.id);
+		} else {
+			const updatedItem = { ...existingitem, amount: existingitem.amount - 1 }
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount
